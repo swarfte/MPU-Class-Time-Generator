709abd78a571635fc230bee20c8e86e722a30d27
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2023-06-14 22:27:58 +0800

    File: add the TableSelector

---
+++ parseData/Selector.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from overrides import override
+import bs4
+
+
+class AbstractSelector(ABC):
+    """
+    Abstract class for selecting data from the given data
+    """
+
+    def __init__(self, data: str | list | dict) -> None:
+        self.data: str | list | dict = data
+
+    @abstractmethod
+    def filter(self) -> str | list | dict:
+        """
+        Filter the original data
+        :return: filtered data
+        """
+        pass
+
+
+class TableSelector(AbstractSelector):
+    def __init__(self, data: str, expression: str) -> None:
+        super().__init__(data)
+        self.expression: str = expression
+
+    @override
+    def filter(self) -> bs4.ResultSet:
+        """
+        Filter the original data
+        :return: filtered data
+        """
+        soup: bs4.BeautifulSoup = bs4.BeautifulSoup(self.data, features="html.parser")
+        table: bs4.ResultSet = soup.find_all('table')
+        return table
